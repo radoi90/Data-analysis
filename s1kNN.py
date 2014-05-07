1d09006f612f0e9160e3b973dac52da1897704b7
Emil <eculic17@gmail.com>
2014-05-07 17:02:53 +0100

    full pipeline data -> split -> CV k fold -> KNN

--- s1kNN.py
+++ s1kNN.py
@@ -3,11 +3,12 @@ import csv
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import cross_validation
 from sklearn import preprocessing
+from sklearn.multiclass import OneVsRestClassifier
 
 data = []
 
 #read data from csv file
-with open('g10.csv', 'rb') as csvfile:
+with open('correct_gestures_plain.csv', 'rb') as csvfile:
 	datareader = csv.reader(csvfile, delimiter=',')
 	for row in datareader:
 		data.append(map(float, row))
@@ -16,45 +17,51 @@ data = np.array(data)
 
 #scale data s.t all features have zero mean and unit variance
 gestures_X = data[:,1:]
+gestures_y = np.array(map(int,data[:,0]))
 gestures_X_scaled = preprocessing.scale(gestures_X)
 
-#train a 1vsAll KNN classifier for each gesture
-labels = [23, 24, 23, 22, 22, 22, 22, 21, 23, 22]
-classifiers=[]
-for n in range (2, 11):
-	m = []
-	s = []
-	for gesture in range(0,10):
-		#compute the labels
-		gestures_y = []
-		for i in range (0,10):
-			for j in range(0,labels[i]):
-				#gestures_y.append(i+1)
-				gestures_y.append(1 if i == gesture else 0)
-		gestures_y = np.array(gestures_y)
+#split the data in training and test sets
+gestures_X_train, gestures_X_test, gestures_y_train, gestures_y_test = \
+	cross_validation.train_test_split(gestures_X_scaled, gestures_y, test_size=0.1, random_state=0)
 
-		gestures_X_train, gestures_X_test, gestures_y_train, gestures_y_test = cross_validation.train_test_split(gestures_X_scaled, gestures_y, test_size=0.1, random_state=0)
+#use k-fold cross validation to determine the best N for KNN
+best_mean = 0
+best_std = 0
+best_N = 0
+print "Beginning KNN k-fold cross-validation"
+for n in range(2,10):
+	knn = KNeighborsClassifier(n_neighbors=n)
+	#use one vs. rest strategy
+	classifiers = OneVsRestClassifier(knn, n_jobs=-1)
+	classifiers.fit(gestures_X_train, gestures_y_train)
 
-		knn = KNeighborsClassifier(n_neighbors=n)
-		scr = cross_validation.cross_val_score(knn, gestures_X_train, gestures_y_train, cv = 5)
-		m.append(scr.mean())
-		s.append(scr.std()*2)
-		classifiers.append(knn)
-		knn.fit(gestures_X_train, gestures_y_train)
-	m = np.array(m)
-	s = np.array(s)
-	print("n=%d\nAccuracy: %0.3f (+/- %0.2f)" % (n,m.mean(), s.sum()))
+	#get the cross validated score
+	scr = cross_validation.cross_val_score(classifiers, gestures_X_train, gestures_y_train, cv = 5)
+	if (scr.mean() > best_mean) or (scr.mean() == best_mean and scr.std() < best_std):
+		best_mean = scr.mean()
+		best_std = scr.std()
+		best_N = n
+	print("n=%d\nAccuracy: %0.3f (+/- %0.2f)" % (n,scr.mean(), scr.std()*2))
 
-	#score each gesture by using the probability of belonging to its class
-	scores = []
-	index = 0
-	for i in range(0,10):
-		for j in range(0,labels[i]):
-			scores.append(int(10*classifiers[i].predict_proba(gestures_X_scaled[index])[0][1]))
-			index += 1
+print ("Using KNN with N=%d" % (best_N))
+#use KNN with n = 5, determined through n-fold cross validation
+knn = KNeighborsClassifier(n_neighbors=best_N)
+#use one vs. rest strategy
+classifiers = OneVsRestClassifier(knn, n_jobs=-1)
+classifiers.fit(gestures_X_train, gestures_y_train)
+
+#score each gesture by using the probability of belonging to its class
+scores = []
+index = 0
+for i in range(0,len(gestures_y)):
+	#pick the estimator for the correct class
+	class_estimator = classifiers.estimators_[gestures_y[i]-1]
+	#get the probability of belonging to that class (being labeled 1)
+	prob = class_estimator.predict_proba(gestures_X_scaled[i])[0][1]
+	scores.append(10*prob)
 
 with open('s1knn_scores.csv', 'wb') as csvfile:
     scorewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for score in scores:
-			scorewriter.writerow([score])
+			scorewriter.writerow([int(score)])
