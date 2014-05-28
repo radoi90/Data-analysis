344d986e5ce26eb770a58cc456f44ecff3649c47
Emil <eculic17@gmail.com>
2014-05-28 18:37:20 +0100

    histos for report

--- histos.py
+++ histos.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pylab as P
 import csv
+
 #      0,      1,   2,        3,             4,        5,             6,  7
 #Gesture,Control,Name,Score_knn,Score_knn_plus,Score_svm,Score_svm_plus,Avg
 data = []
-bins = range(-1,11)
+bins = range(-1,12)
 
 #read data from csv file
 with open('scores.csv', 'rb') as csvfile:
@@ -13,56 +14,42 @@ with open('scores.csv', 'rb') as csvfile:
     data.append(map(float, row))
 
 data = np.array(data)
-data = np.array([d for d in data if d[2] > 0])
+controls = np.array([d for d in data if d[1] == 1 and d[0] != 1 and d[0] != 7])
+p = np.array([d for d in data if d[1] == 0 and d[0] != 1 and d[0] != 7])
+patients = np.copy(p)
+
+for i in range (1,14):
+    patients = np.vstack((patients, p))
 
 P.figure(1)
-P.title('KNN_all_on_correct')
-P.hist(data[:,3],bins=bins)
-
-P.figure(2)
-P.title('KNN_all_on_all')
-P.hist(data[:,4],bins=bins)
+P.subplot()
+P.title('KNN')
+P.hist([controls[:,3], patients[:,3]],bins=bins,align='left')
 
 P.figure(3)
-P.title('SVM_all_on_correct')
-P.hist(data[:,5],bins=bins)
-
-P.figure(4)
-P.title('SVM_all_on_all')
-P.hist(data[:,6],bins=bins)
+P.title('SVM')
+P.hist([controls[:,5], patients[:,5]],bins=bins,align='left')
 
 P.figure(5)
 P.title('AVG_all')
-P.hist(data[:,6],bins=bins)
-
+P.hist([controls[:,7], patients[:,7]],bins=bins,align='left')
+"""
 fig = 6
 for g in range(1,11):
-  gesture_data = np.array([d for d in data if d[0] == g])
-  g = str(g)
+  f, (ax1, ax2, ax3) = P.subplots(1, 3, sharey=True)
+  P.title(str(g))
 
-  P.figure(fig)
-  fig +=1
-  P.title('KNN_'+g+'_on_correct')
-  P.hist(gesture_data[:,3],bins=bins)
+  gesture_controls = np.array([d for d in controls if d[0] == g])
+  gesture_patients = np.array([d for d in patients if d[0] == g])
 
-  P.figure(fig)
-  fig +=1
-  P.title('KNN_'+g+'_on_all')
-  P.hist(gesture_data[:,4],bins=bins)
+  if gesture_patients.size > 0:
+    ax1.set_title('KNN')
+    ax1.hist([gesture_controls[:,3], gesture_patients[:,3]],bins=bins,align='left')
 
-  P.figure(fig)
-  fig +=1
-  P.title('SVM_'+g+'_on_correct')
-  P.hist(gesture_data[:,5],bins=bins)
+    ax2.set_title('SVM')
+    ax2.hist([gesture_controls[:,5], gesture_patients[:,5]],bins=bins,align='left')
 
-  P.figure(fig)
-  fig +=1
-  P.title('SVM_'+g+'_on_all')
-  P.hist(gesture_data[:,6],bins=bins)
-
-  P.figure(fig)
-  fig +=1
-  P.title('AVG_'+g)
-  P.hist(gesture_data[:,6],bins=bins)
-
+    ax3.set_title('AVG')
+    ax3.hist([gesture_controls[:,7], gesture_patients[:,7]],bins=bins,align='left')
+"""
 P.show()
