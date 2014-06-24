99f9c88a504c8797c41e1c53c4a73c870419b825
Emil <eculic17@gmail.com>
2014-06-24 12:23:15 +0100

    fiddled with histograms, added score

--- histos.py
+++ histos.py
@@ -2,11 +2,10 @@ import numpy as np
 import pylab as P
 import csv
 
-#      0,      1,   2,        3,             4,        5,             6,  7
-#Gesture,Control,Name,Score_knn,Score_knn_plus,Score_svm,Score_svm_plus,Avg
+#      0,      1,   2,        3,        4,  5
+#Gesture,Control,Name,Score_knn,Score_svm,Avg
 data = []
 bins = range(-1,12)
-
 #read data from csv file
 with open('scores.csv', 'rb') as csvfile:
   datareader = csv.reader(csvfile, delimiter=',')
@@ -24,16 +23,16 @@ for i in range (1,14):
 P.figure(1)
 P.subplot()
 P.title('KNN')
-P.hist([controls[:,3], patients[:,3]],bins=bins,align='left')
+P.hist([controls[:,3], patients[:,3]],bins=bins,align='right')
 
 P.figure(3)
 P.title('SVM')
-P.hist([controls[:,5], patients[:,5]],bins=bins,align='left')
+P.hist([controls[:,4], patients[:,4]],range=[0,10],align='right')
 
 P.figure(5)
 P.title('AVG_all')
-P.hist([controls[:,7], patients[:,7]],bins=bins,align='left')
-"""
+P.hist([controls[:,5], patients[:,5]],range=[0,10],align='right')
+
 fig = 6
 for g in range(1,11):
   f, (ax1, ax2, ax3) = P.subplots(1, 3, sharey=True)
@@ -44,12 +43,12 @@ for g in range(1,11):
 
   if gesture_patients.size > 0:
     ax1.set_title('KNN')
-    ax1.hist([gesture_controls[:,3], gesture_patients[:,3]],bins=bins,align='left')
+    ax1.hist([gesture_controls[:,3], gesture_patients[:,3]],bins=bins,align='right')
 
     ax2.set_title('SVM')
-    ax2.hist([gesture_controls[:,5], gesture_patients[:,5]],bins=bins,align='left')
+    ax2.hist([gesture_controls[:,4], gesture_patients[:,4]],bins=bins,align='right')
 
     ax3.set_title('AVG')
-    ax3.hist([gesture_controls[:,7], gesture_patients[:,7]],bins=bins,align='left')
-"""
+    ax3.hist([gesture_controls[:,5], gesture_patients[:,5]],bins=bins,align='right')
+
 P.show()
